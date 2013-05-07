a4d4186b68f478453f4b46f9db3a32a779cec84a
sgibbon <stephen@gibbonspace.com>
2013-05-07 15:07:12 +1000

    added styling

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var app = {
 
     initialize: function() {
         var self = this;
+        this.registerEvents();
         this.store = new MemoryStore(function () {
             $('body').html(new HomeView(self.store).render().el);
         });
@@ -13,7 +14,29 @@ var app = {
         } else {    
             alert(title ? (title + ':' + message) : message);
         }
-    }
+    },
+
+    registerEvents: function() {
+        var self = this;
+        // Check of browser supports touch events...
+        if (document.documentElement.hasOwnProperty('ontouchstart')) {
+            // ... if yes: register touch event listener to change the "selected" state of the item
+            $('body').on('touchstart', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('touchend', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        } else {
+            // ... if not: register mouse events instead
+            $('body').on('mousedown', 'a', function(event) {
+                $(event.target).addClass('tappable-active');
+            });
+            $('body').on('mouseup', 'a', function(event) {
+                $(event.target).removeClass('tappable-active');
+            });
+        }
+    },
 };
 
 app.initialize();
